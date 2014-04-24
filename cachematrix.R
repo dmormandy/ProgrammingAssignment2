5b3e43ace58286bf4a3fbf126072bbe355ce7735
dmormandy <danielle.d.ormandy@ericsson.com>
2014-04-24 19:00:55 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two Functions to create a vector containing functions to store a numeric vector 
+## and cache its inverse
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a numeric matrix and returns in a list functions to:
+## 1. Set the value to memory
+## 2. Get the value from memory
+## 3. Set the value of the inverse of the matrix to memory
+## 4. Get the value of the inverse of the matrix from memory
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      ## Initialise the inverse to NULL
+  inv_x <- NULL
+      ## Create function "set" to set x to input value and inv to NULL
+  set <- function(y) {
+    x <<- y
+    inv_x <<- NULL
+  }
+      ## Create function "get" to get value of x
+  get <- function() x
+      ## Create function "setinverse" to set inv_x to the inverse of x
+  setinverse <- function(x) inv_x <<- solve(x) 
+      ## Create function "getinverse" to return value of inv_x  
+  getinverse <- function() inv_x 
+      ## Put all (named) functions into a list and return
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+## cacheSolve returns the inverse of a numeric matrix by:
+## 1. Checking to see if the inverse has already been calculated. If so, it gets the mean from the cache 
+## 2. Otherwise, it calculates the inverse of the numeric matrix 
+##    and sets the value of the mean in the cache via the setmean function.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(flist, x) {
+        ## Inputs are a list of functions "flist" (created by makeCacheMatrix) and the numeric matrix "x"
+        ## Attempt to get the inverse matrix
+  inv_x <- flist$getinverse()
+        ## If inverse matrix already exists then return it
+  if(!is.null(inv_x)) { 
+    message("getting cached data")
+    return(inv_x) 
+        ## Otherwise set the inverse and return it
+  } else inv <- flist$setinverse(x)
+    message("setting inverse and storing in cache")
+    return(inv_x)
 }
